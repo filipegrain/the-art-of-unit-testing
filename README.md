db5a9a138f2e72ede610fe6af99a5d93e4fd8ec6
filipegrain <flprocha92@gmail.com>
2024-05-20 22:30:58 -0300

    feat: finished intro

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Studying javascript unit testing with Jest, using the book The Art of Unit Testing.
+
+npm run test // equals calling 'npm run jest' run tests one time
+npm run testw // equals calling 'npm run jest --watch' watch file changes to run the tests
--- __tests__/password-verifier0.spec.js
+++ __tests__/password-verifier0.spec.js
@@ -1,11 +1,14 @@
 class PasswordVerifier1 {
-      constructor () {
+      constructor() {
             this.rules = []
       }
       addRule(rule) {
             this.rules.push(rule)
       }
       verify(input) {
+            if (this.rules.length === 0) {
+                  throw new Error('There are no rules configured');
+            }
             const errors = []
             this.rules.forEach(rule => {
                   const result = rule(input)
@@ -17,53 +20,79 @@ class PasswordVerifier1 {
       }
 }
 
+const makeVerifier = () => new PasswordVerifier1();
+const passingRule = (input) => ({ passed: true, reason: '' });
+
+const makeVerifierWithPassingRule = () => {
+      const verifier = makeVerifier();
+      verifier.addRule(passingRule);
+      return verifier;
+};
+
+const makeVerifierWithFailedRule = (reason) => {
+      const verifier = makeVerifier();
+      const fakeRule = input => ({ passed: false, reason: reason });
+      verifier.addRule(fakeRule);
+      return verifier;
+};
+
 describe('PasswordVerifier', () => {
-      let verifier
-      beforeEach(() => verifier = new PasswordVerifier1())
       describe('with a failing rule', () => {
-            let errors
-            beforeEach(() => {
-                  verifier.addRule(makeFailingRule('fake reason'))
-                  errors = verifier.verify('any value');
-            })
-            it('has an error message based on the rule.reason', () => {
-                  expect(errors[0]).toContain('fake reason')
-            })
+            test('has an error message based on the rule.reason', () => {
+                  const verifier = makeVerifierWithFailedRule('fake reason');
+                  const errors = verifier.verify('any input');
+                  expect(errors[0]).toContain('fake reason');
+            });
             it('has exactly one error', () => {
+                  const verifier = makeVerifierWithFailedRule('fake reason');
+                  const errors = verifier.verify('any input');
                   expect(errors.length).toBe(1);
-            })
-      })
+            });
+      });
       describe('with a passing rule', () => {
-            let errors;
-            beforeEach(() => {
-              verifier.addRule(makePassingRule());
-              errors = verifier.verify('any value');
-            });
             it('has no errors', () => {
-              expect(errors.length).toBe(0);
+                  const verifier = makeVerifierWithPassingRule();
+                  const errors = verifier.verify('any input');
+                  expect(errors.length).toBe(0);
             });
-          });
-          describe('with a failing and a passing rule', () => {
-            let errors;
-            beforeEach(() => {
-              verifier.addRule(makePassingRule());
-              verifier.addRule(makeFailingRule('fake reason'));
-              errors = verifier.verify('any value');
-            });
+      });
+      describe('with a failing and a passing rule', () => {
             it('has one error', () => {
-              expect(errors.length).toBe(1);
+                  const verifier = makeVerifierWithFailedRule('fake reason');
+                  verifier.addRule(passingRule);
+                  const errors = verifier.verify('any input');
+                  expect(errors.length).toBe(1);
             });
             it('error text belongs to failed rule', () => {
+                  const verifier = makeVerifierWithFailedRule('fake reason');
+                  verifier.addRule(passingRule);
+                  const errors = verifier.verify('any input');
                   expect(errors[0]).toContain('fake reason');
-                });
-              });
-})
+            });
+      });
+});
 
-const makeFailingRule = (reason) => {
-      return (input) => {
-        return { passed: false, reason: reason };
+// Listing 2.21 Using test.each
+const oneUpperCaseRule = (input) => {
+      return {
+            passed: (input.toLowerCase() !== input),
+            reason: 'at least one upper case needed'
       };
-    };
-    const makePassingRule = () => (input) => {
-      return { passed: true, reason: '' };
-    };
+};
+describe('one uppercase rule', () => {
+      test.each([
+            ['Abc', true],
+            ['aBc', true],
+            ['abc', false],
+      ])
+            ('given %s, %s ', (input, expected) => {
+                  const result = oneUpperCaseRule(input);
+                  expect(result.passed).toEqual(expected);
+            });
+});
+
+// Listing 2.26 Using expect().toThrowError()
+test('verify, with no rules, throws exception', () => {
+      const verifier = makeVerifier();
+      expect(() => verifier.verify('any input')).toThrowError(/no rules configured/);
+});
